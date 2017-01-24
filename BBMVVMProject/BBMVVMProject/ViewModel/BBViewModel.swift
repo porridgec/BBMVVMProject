8859e685d1596c79270dbce8fa6076831063df3a
Hahn.Chan <porridgec@gmail.com>
2017-01-24 14:21:15 +0800

    Import SnapKit for UI layout and Moya for network requests.

--- BBMVVMProject/BBMVVMProject/ViewModel/BBViewModel.swift
+++ BBMVVMProject/BBMVVMProject/ViewModel/BBViewModel.swift
@@ -11,14 +11,21 @@ import UIKit
 class BBViewModel: NSObject {
     
     let navigationBus: BBNavigationEventBus = BBNavigationEventBus.sharedEventBus
-    let params: [String: AnyObject]!
-    let navigationTitle: String!
+    let params: [String: AnyObject]
+    let navigationTitle: String
+    let shouldHideNavigationBar: Bool
+    let shouldHideStatusBar: Bool
     
     init(withParams: [String: AnyObject]) {
+        
         params = withParams
         navigationTitle = (params["navigationTitle"] as? String) ?? ""
+        shouldHideNavigationBar = (params["shouldHideNavigationBar"] as? Bool) ?? false
+        shouldHideStatusBar = (params["shouldHideNavigationBar"] as? Bool) ?? false
+        
         super.init()
         initialize()
+        
     }
     
     func initialize() {} //override this function in subclasses
--- BBMVVMProject/Pods/Target Support Files/Pods-BBMVVMProject/Pods-BBMVVMProject-frameworks.sh
+++ BBMVVMProject/Pods/Target Support Files/Pods-BBMVVMProject/Pods-BBMVVMProject-frameworks.sh
@@ -84,12 +84,18 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveSwift/ReactiveSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/Moya/Moya.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveCocoa/ReactiveCocoa.framework"
   install_framework "$BUILT_PRODUCTS_DIR/ReactiveSwift/ReactiveSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Result/Result.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
 fi
